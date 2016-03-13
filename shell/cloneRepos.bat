001cf9d6859d49a08b539fbf14bbc51075e839d8
Harrison <harrisonbeachey@gmail.com>
2016-03-12 21:12:05 -0800

    update helper scripts

--- shell/cloneRepos.bat
+++ shell/cloneRepos.bat
@@ -4,12 +4,26 @@ cd ..
 echo "Cloning Repos..."
 git clone https://github.com/innatewonder/onyx.git
 git clone https://github.com/innatewonder/mythril.git
+git clone https://github.com/innatewonder/opal.git
 
 echo "Linking Directories..."
 cd mythril\include
 mklink /D networking ..\..\onyx\include\networking
+mklink /D audio ..\..\opal\include\audio
 cd ..\src
 mklink /D networking ..\..\onyx\src\networking
-cd ..\..\
+mklink /D audio ..\..\opal\src\audio
+
+cd ..\..\opal\include
+mklink /D networking ..\..\onyx\include\networking
+mklink /D core ..\..\mythril\include\core
+cd ..\src
+mklink /D networking ..\..\onyx\src\networking
+mklink /D core ..\..\mythril\src\core
+
+cd ../../onyx/include
+mklink /D core ..\..\mythril\include\core
+cd ../src
+mklink /D core ..\..\mythril\src\core
 
 @echo on
--- shell/cloneRepos.sh
+++ shell/cloneRepos.sh
@@ -4,10 +4,24 @@ cd ..
 echo "Cloning Repos..."
 git clone https://github.com/innatewonder/onyx.git
 git clone https://github.com/innatewonder/mythril.git
+git clone https://github.com/innatewonder/opal.git
 
 echo "Linking Directories..."
 cd mythril/include
 ln -s ../../onyx/include/networking/
+ln -s ../../opal/include/audio/
 cd ../src
 ln -s ../../onyx/src/networking/
-cd ../../
+ln -s ../../opal/src/audio/
+
+cd ../../opal/include
+ln -s ../../onyx/include/networking/
+ln -s ../../mythril/include/core
+cd ../src
+ln -s ../../onyx/src/networking/
+ln -s ../../mythril/src/core
+
+cd ../../onyx/include
+ln -s ../../mythril/include/core
+cd ../src
+ln -s ../../mythril/src/core
